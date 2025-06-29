53fe7bf3e7f8c2fd4bee1ded55b4d67300ec417a
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 10:53:06 +0530

    Update ApplyDiscount.sql

--- Week2/PLSQL exercises/Exercise 1/Scenario 1/ApplyDiscount.sql
+++ Week2/PLSQL exercises/Exercise 1/Scenario 1/ApplyDiscount.sql
@@ -1,1 +1,16 @@
+DECLARE
+    CURSOR c_discount IS
+        SELECT l.LoanID
+        FROM Customers c
+        JOIN Loans l ON c.CustomerID = l.CustomerID
+        WHERE TRUNC(MONTHS_BETWEEN(SYSDATE, c.DOB) / 12) > 60;
 
+BEGIN
+    FOR rec IN c_discount LOOP
+        UPDATE Loans
+        SET InterestRate = InterestRate * 0.99
+        WHERE LoanID = rec.LoanID;
+    END LOOP;
+
+    COMMIT;
+END;
