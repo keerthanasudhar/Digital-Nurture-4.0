e08587c8746cfdbdf704919afa76f36fcc3d301f
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 10:56:36 +0530

    Update Is Vip.sql

--- Week2/PLSQL exercises/Exercise 1/Scenario 2/Is Vip.sql
+++ Week2/PLSQL exercises/Exercise 1/Scenario 2/Is Vip.sql
@@ -1,1 +1,9 @@
+ alter table customers add isVip varchar(5);
 
+ begin
+  for rec in (select * from customers where balance >10000) loop
+    update customers set isVip='true' where customerid=rec.customerid;
+  end loop;
+  commit;
+ end;
+ /
