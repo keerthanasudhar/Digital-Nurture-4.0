11fca8c3142b1657220460a47f9ba1ae9f4e1b08
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 11:11:14 +0530

    Update CalculatorTest.java

--- Week2/TDD Using JUnit and Mockito/Exercise 1/CalculatorTest.java
+++ Week2/TDD Using JUnit and Mockito/Exercise 1/CalculatorTest.java
@@ -1,1 +1,13 @@
+package com.example.JUnitDemo;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        System.out.println("Executing testAddition()");
+        assertEquals(5, result);
+    }
+}
