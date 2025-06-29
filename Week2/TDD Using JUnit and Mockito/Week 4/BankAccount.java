ef0b154c88e4cbff6d155a8ad588809d9afae20d
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 22:11:32 +0530

    Update BankAccount.java

--- Week2/TDD Using JUnit and Mockito/Week 4/BankAccount.java
+++ Week2/TDD Using JUnit and Mockito/Week 4/BankAccount.java
@@ -1,1 +1,23 @@
-
+package com.example.JUnitDemo;
+public class BankAccount {
+  private double balance;
+	public BankAccount(double initialAmt)
+	{
+		this.balance=initialAmt;
+	}
+	public double getBalance()
+	{
+		return balance;
+	}
+	public void withDraw(double amt)
+	{
+		if(amt>0 && amt<balance)
+		{
+			balance-=amt;
+		}
+	}
+	public void deposit(double amt)
+	{
+		balance+=amt;
+	}
+}
