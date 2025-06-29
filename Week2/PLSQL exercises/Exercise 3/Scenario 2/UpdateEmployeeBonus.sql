490116e0d1f8f2933e5f9ad7ba4da3d02943580c
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 11:04:44 +0530

    Update UpdateEmployeeBonus.sql

--- Week2/PLSQL exercises/Exercise 3/Scenario 2/UpdateEmployeeBonus.sql
+++ Week2/PLSQL exercises/Exercise 3/Scenario 2/UpdateEmployeeBonus.sql
@@ -1,1 +1,6 @@
-
+ create or replace procedure UpdateEmployeeBonus(dept in varchar2,bonus_percent in number) is
+begin
+ update employees set salary=salary + (salary *bonus_percent/100) where department=dept;
+commit;
+end;
+/
