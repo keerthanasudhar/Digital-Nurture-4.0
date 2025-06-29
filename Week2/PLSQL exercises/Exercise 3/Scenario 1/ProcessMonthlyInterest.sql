cff8ba0093b802ad88acf6967a012cd0357b9bf7
keerthanasudhar <keerthanasudhar1115@gmail.com>
2025-06-29 11:02:35 +0530

    Update ProcessMonthlyInterest.sql

--- Week2/PLSQL exercises/Exercise 3/Scenario 1/ProcessMonthlyInterest.sql
+++ Week2/PLSQL exercises/Exercise 3/Scenario 1/ProcessMonthlyInterest.sql
@@ -1,1 +1,6 @@
-
+create or replace procedure ProcessMonthlyInterest is 
+begin
+ update accounts set balance=balance +(balance*(1/100)) where accounttype='savings';
+end;
+/
+exec ProcessMonthlyInterest;
